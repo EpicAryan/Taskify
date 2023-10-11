3d38a53a72c64034b1c3290e93acd7747a2a6075
Aryan Kumar <adaryan0811@gmail.com>
2023-10-11 14:04:03 +0530

    feat: Implement update and delete functions in the backend server
    Extend API with endpoints for updating and deleting records, enhancing data management capabilities

--- backend/controllers/ToDoController.js
+++ backend/controllers/ToDoController.js
@@ -8,11 +8,27 @@ module.exports.getToDo = async (req,res) => {
 module.exports.saveToDo = async (req, res) => {
 
     const{ text } = req.body;
-    ToDoModel.
-    create({ text})
+    ToDoModel
+    .create({ text})
     .then((data) => {
         console.log("Added Successfully");
         console.log(data);
         res.send(data);
     })
+};
+
+module.exports.updateToDo = async (req, res) => {
+    const {_id, text } = req.body;
+    ToDoModel
+    .findByIdAndUpdate(_id, {text})
+    .then(() => res.send("Updated Successfully"))
+    .catch((err) => console.log(err));
+};
+
+module.exports.deleteToDo = async (req, res) => {
+    const {_id} = req.body;
+    ToDoModel
+    .findByIdAndDelete(_id)
+    .then(() => res.send("Deleted Successfully"))
+    .catch((err) => console.log(err));
 };
--- backend/routes/ToDoRoute.js
+++ backend/routes/ToDoRoute.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { getToDo, saveToDo  } = require("../controllers/ToDoController");
+const { getToDo, saveToDo , updateToDo, deleteToDo} = require("../controllers/ToDoController");
 
 const router = express.Router();
 
-router.get('/get', getToDo);
+router.get('/', getToDo);
 router.post('/save', saveToDo);
+router.post('/update', updateToDo);
+router.post('/delete', deleteToDo);
 module.exports = router;
